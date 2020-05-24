3f0be8af0a95063d9e4c34d35bae92bf2d21c04c
Rick Donato <rick@donato.me.uk>
2020-05-24 22:55:02 +0100

    Sort imports

--- acl_auditor/auditor.py
+++ acl_auditor/auditor.py
@@ -1,28 +1,29 @@
-from pybatfish.client.commands import *
-from pybatfish.question.question import load_questions
 import argparse
 import logging
+import os
 
+from dotenv import load_dotenv
+from helpers import create_acl_from_yaml, read_file
 from pybatfish.client.commands import *
 from pybatfish.datamodel.flow import HeaderConstraints, PathConstraints
 from pybatfish.question import bfq
 from pybatfish.question.question import load_questions
 
-from helpers import read_file, create_acl_from_yaml
+load_dotenv()
 
 logging.getLogger("pybatfish").setLevel(logging.CRITICAL)
 
 
 class DeviceFlows:
-    def __init__(self, config_file, flows_file, acl_name, hostname, batfish_ip):
-        self.init_session(batfish_ip)
+    def __init__(self, config_file, flows_file, acl_name, hostname, batfish_host):
+        self.init_session(batfish_host)
         self.config_file = config_file
         self.flows_file = flows_file
         self.hostname = hostname
         self.acl_name = acl_name
 
-    def init_session(self, batfish_ip):
-        bf_session.host = batfish_ip
+    def init_session(self, batfish_host):
+        bf_session.host = batfish_host
         load_questions()
 
     def _create_base_snapshot(self):
@@ -42,10 +43,9 @@ class DeviceFlows:
     def compare_filters(self):
         self._create_base_snapshot()
         self._create_reference_snapshot()
-        answer = bfq.compareFilters().answer(
+        self.answer = bfq.compareFilters().answer(
             snapshot="base", reference_snapshot="reference"
         )
-        print(answer.frame())
 
 
 if __name__ == "__main__":
@@ -59,7 +59,10 @@ if __name__ == "__main__":
     flows_file = args["flows"]
     acl_name = args["acl_name"]
 
+    batfish_host = os.getenv("BATFISH_SERVICE_HOST")
+
     device_flows = DeviceFlows(
-        config_file, flows_file, acl_name, "fw1", "172.29.236.139"
+        config_file, flows_file, acl_name, "fw1", batfish_host
     )
     device_flows.compare_filters()
+    print(device_flows.answer.frame())
